641ba45e43e071969a276b08e75927458b8fa194
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2023-01-14 20:16:46 -0700

    [ADD] Terminada la maquetacion del formulario de checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,3 +1,22 @@
+import '../styles/components/Checkout.css'
+
 export const Checkout = () => {
-  return <h1>Checkout</h1>;
+    return (
+        <div className="Checkout">
+            <div className="Checkout-content">
+                <h3>Lista de pedidos</h3>
+                <div className="Checkout-item">
+                    <div className="Checkout-element">
+                        <h4>Item name</h4>
+                        <span>$10</span>
+                    </div>
+                    <button type="button">Eliminar</button>
+                </div>
+            </div>
+            <div className="Checkout-sidebar">
+                <h3>Precio Total: $10</h3>
+                <button type="button">Continuar con el pedido</button>
+            </div>
+        </div>
+    );
 };
