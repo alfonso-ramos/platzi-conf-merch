2fa07bb027446b76e1406c6b34fed3dc4d6c7e47
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2023-01-14 20:33:26 -0700

    [ADD] Terminada la maquetacion del flujo de pago

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,3 +1,14 @@
+import '../styles/components/Payment.css'
+
 export const Payment = () => {
-  return <h1>Payment</h1>;
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido</h3>
+        <div className="Payment-button">
+          Pago con Paypal
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,3 +1,15 @@
+import '../styles/components/Success.css'
+
 export const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu ubicacion</span>
+        <div className="Success-map">
+          Google-map
+        </div>
+      </div>
+    </div>
+  );
 };
